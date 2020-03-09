a144133876182bc542020941bf25a539b812cf7e
lustered <smellslikelavenders@gmail.com>
2020-03-09 12:01:07 -0400

    correct file

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,65 @@
 #include <Arduino.h>
-// 1
-#define button1Pin  13
-#define button2Pin  12
-#define button3Pin  11
-// 2 
-#define led1Pin 6
-#define led2Pin 5
-#define led3Pin 4
-#define led4Pin 3
-// 3
-int button1_state = 0;
-int button2_state = 0;
-int button3_state = 0;
+const int button3Pin = 11; // the number of the pushbutton pin
+const int button2Pin = 12;
+const int button1Pin = 13;
 
-void setup() {
-  Serial.begin(9600);
-  // leds
-  pinMode(led1Pin,OUTPUT);
-  pinMode(led2Pin,OUTPUT);
-  pinMode(led3Pin,OUTPUT);
-  pinMode(led4Pin,OUTPUT);
-  // button pins
-  pinMode(button3Pin,INPUT);
-  pinMode(button2Pin,INPUT);
-  pinMode(button1Pin,INPUT);
+const int led1_Pin = 6; // the number of the LED pin
+const int led2_Pin = 5;
+const int led3_Pin = 4;
+const int led4_Pin = 3;
+
+// variables will change:
+int button1State = 0;
+int button2State = 0;
+int button3State = 0;
+// prototypes
+void manage_led(int button, int led);
+int logic_gate(int gate, int a, int b);
+
+void setup()
+{
+  // initialize the LED pin as an output:
+  pinMode(led1_Pin, OUTPUT);
+  pinMode(led2_Pin, OUTPUT);
+  pinMode(led3_Pin, OUTPUT);
+  pinMode(led4_Pin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(button1Pin, INPUT);
+  pinMode(button2Pin, INPUT);
+  pinMode(button3Pin, INPUT);
+}
+
+void loop()
+{
+  // read the state of the pushbutton value:
+  button1State = digitalRead(button1Pin);
+  button2State = digitalRead(button2Pin);
+  button3State = digitalRead(button3Pin);
+
+
+  manage_led(button3State,led3_Pin);
+  manage_led(button2State,led2_Pin);
+  manage_led(button1State,led1_Pin);
+
+  int get_state = logic_gate(1,button1State, button2State);
+  if(button3State == HIGH){
+    get_state = 1 - get_state;
+  }
+  manage_led(get_state,led4_Pin);
+}
+
+// turn led on/off function because we don't like bad code
+void manage_led(int button, int led){
+  if(button == HIGH){
+    digitalWrite(led,HIGH);
+  }
+  else{
+      digitalWrite(led,LOW);
+  }
 
 }
 
-void loop() {
-  button3_state = digitalRead(button3Pin);
-  button2_state = digitalRead(button2Pin);
-  button1_state = digitalRead(button1Pin);
-
-  Serial.println(button1_state);
-  Serial.println(button2_state);
-  Serial.println(button3_state);
-  delay(1000);
-
-  if(button3_state == HIGH){
-    digitalWrite(led1Pin, HIGH);
-  }
-  else{
-    digitalWrite(led1Pin, LOW);
-  }
-  if(button2_state == HIGH){
-    digitalWrite(led2Pin, HIGH);
-  }
-  else{
-    digitalWrite(led2Pin, LOW);
-  }
-
-}
-
+// logic gate function
 int logic_gate(int gate, int a, int b)
 {
   int ret = 0;
@@ -74,7 +82,6 @@ int logic_gate(int gate, int a, int b)
       ret = 1;
 
     break;
-
   }
   return ret;
   /* end of switch */
